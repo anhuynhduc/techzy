612f00ffa40b6025da41efe9d37c45edcf358cac
anhuynhduc <ansuu552002@gmail.com>
2025-05-04 18:21:03 +0700

    new commit

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__8ebb6d4b._.css",
-  "static/chunks/_4df926a3._.js"
+  "static/chunks/_3c4d65f0._.js"
 ],
     source: "dynamic"
 });
