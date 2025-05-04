34f76794d1d1a1db66fdf4a7abef3931d989fe3d
anhuynhduc <ansuu552002@gmail.com>
2025-05-04 18:34:32 +0700

    new commit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,6 @@
 import { createStore } from 'redux';
 import rootReducer from './reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const composedEnhancers = composeWithDevTools();
-
-const store = createStore(rootReducer, composedEnhancers);
+const store = createStore(rootReducer);
 
 export default store;
